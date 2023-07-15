738674043b893832badf80ec5a99a294988350a4
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-07-15 15:16:54 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+num1=15
+num2=5
+print("Addition of ",num1,"and",num2,"is",num1+num2)
+print("Substraction of ",num1,"and",num2,"is",num1-num2)
+print("Multiplication of ",num1,"and",num2,"is",num1*num2)
+print("Division of ",num1,"and",num2,"is",num1/num2)
+print("Remainder of ",num1,"and",num2,"is",num1%num2)
